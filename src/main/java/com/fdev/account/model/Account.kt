45632f22fdff3e49f8ac6856887aed92b00f1de7
Burak Dursun <burakdursunn.bd@gmail.com>
2023-09-05 17:49:23 +0300

    Model classes is updated.

--- src/main/java/com/fdev/account/model/Account.kt
+++ src/main/java/com/fdev/account/model/Account.kt
@@ -11,6 +11,7 @@ data class Account(
         @GeneratedValue(generator = "UUID")
         @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
         val id: String? = "",
+
         val balance: BigDecimal? = BigDecimal.ZERO,
         val creationDate: LocalDateTime,
 
@@ -18,4 +19,26 @@ data class Account(
         @JoinColumn(name = "customer_id", nullable = false)
         val customer: Customer?,
 
-        )
+        ) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Account
+
+        if (id != other.id) return false
+        if (balance != other.balance) return false
+        if (creationDate != other.creationDate) return false
+        if (customer != other.customer) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + (balance?.hashCode() ?: 0)
+        result = 31 * result + creationDate.hashCode()
+        result = 31 * result + (customer?.hashCode() ?: 0)
+        return result
+    }
+}
