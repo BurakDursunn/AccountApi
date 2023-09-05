dabb91f6f5af6eb565bccf70e8c4a7bcc52892a3
Burak Dursun <burakdursunn.bd@gmail.com>
2023-09-05 21:17:48 +0300

    Dto classes is added.

---
+++ src/main/java/com/fdev/account/dto/AccountDto.kt
@@ -0,0 +1,15 @@
+package com.fdev.account.dto
+
+import com.fdev.account.model.Transaction
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+data class AccountDto(
+
+        val id: String?,
+        val balance: BigDecimal?,
+        val creationDate: LocalDateTime,
+        val customer: AccountCustomerDto?,
+        val transactions: Set<TransactionDto>?
+
+)
---
+++ src/main/java/com/fdev/account/dto/CustomerDto.kt
@@ -0,0 +1,8 @@
+package com.fdev.account.dto
+
+data class CustomerDto(
+
+        val id: String?,
+        val name: String?,
+        val surname: String?
+)
