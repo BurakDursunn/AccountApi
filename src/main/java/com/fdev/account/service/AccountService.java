b036a9b9aad52f5facd443aff741732ad8efdf2d
Burak Dursun <burakdursunn.bd@gmail.com>
2023-09-09 20:48:57 +0300

    Service classes is completed

--- src/main/java/com/fdev/account/service/AccountService.java
+++ src/main/java/com/fdev/account/service/AccountService.java
@@ -1,15 +1,64 @@
 package com.fdev.account.service;
 
+import com.fdev.account.dto.AccountDto;
+import com.fdev.account.dto.CreateAccountRequest;
+import com.fdev.account.dto.converter.AccountDtoConverter;
+import com.fdev.account.model.Account;
+import com.fdev.account.model.Customer;
+import com.fdev.account.model.Transaction;
 import com.fdev.account.repository.AccountRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.logging.Logger;
 
 @Service
 public class AccountService {
 
-    private final AccountRepository accountRepository;
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(AccountService.class);
 
-    public AccountService(AccountRepository accountRepository) {
+    private final AccountRepository accountRepository;
+    private final CustomerService customerService;
+    private final AccountDtoConverter converter;
+    private final Clock clock;
+
+
+    public AccountService(AccountRepository accountRepository, CustomerService customerService,
+                          AccountDtoConverter converter, Clock clock) {
         this.accountRepository = accountRepository;
+        this.customerService = customerService;
+        this.converter = converter;
+        this.clock = clock;
+    }
+
+    public AccountDto createAccount(CreateAccountRequest createAccountRequest) {
+        Customer customer = customerService.findCustomerById(createAccountRequest.getCustomerId());
+
+        Account account = new Account(
+                customer,
+                createAccountRequest.getInitialCredit(),
+                getLocalDateTimeNow());
+
+        if (createAccountRequest.getInitialCredit().compareTo(BigDecimal.ZERO) > 0) {
+            Transaction transaction = new Transaction(
+                    createAccountRequest.getInitialCredit(),
+                    getLocalDateTimeNow(),
+                    account);
+
+            account.getTransaction().add(transaction);
+        }
+        return converter.convert(accountRepository.save(account));
+    }
+
+    private LocalDateTime getLocalDateTimeNow() {
+        Instant instant = clock.instant();
+        return LocalDateTime.ofInstant(
+                instant,
+                Clock.systemDefaultZone().getZone());
     }
 
 }
--- src/main/java/com/fdev/account/service/CustomerService.java
+++ src/main/java/com/fdev/account/service/CustomerService.java
@@ -1,17 +1,24 @@
 package com.fdev.account.service;
 
+import com.fdev.account.dto.CustomerDto;
+import com.fdev.account.dto.converter.CustomerDtoConverter;
 import com.fdev.account.exceptions.CustomerNotFoundException;
 import com.fdev.account.model.Customer;
 import com.fdev.account.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final CustomerDtoConverter converter;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, CustomerDtoConverter converter) {
         this.customerRepository = customerRepository;
+        this.converter = converter;
     }
 
     protected Customer findCustomerById(String id){
@@ -19,5 +26,16 @@ public class CustomerService {
                 orElseThrow(()->new CustomerNotFoundException("Customer could not found by id: " + id));
     }
 
+    public CustomerDto getCustomerById(String customerId){
+        return converter.convertToCustomerDto(findCustomerById(customerId));
+    }
+
+    public List<CustomerDto> getAllCustomer() {
+
+        return customerRepository.findAll()
+                .stream()
+                .map(converter::convertToCustomerDto)
+                .collect(Collectors.toList());
+    }
 
 }
